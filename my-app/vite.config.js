8a2e48823164989d9d2118f5bc796cc4706004da
Muhammad Abdullah Wali <abdullahwali79@gmail.com>
2025-09-24 08:14:04 +0500

    submit

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CoolStarsServices/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+  },
+  server: {
+    port: 3000,
   },
 })
